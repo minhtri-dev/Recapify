50de0f53a1a713a659afb81c8456ad998230e0c9
minhtri-dev <minhtritran.dev@gmail.com>
2025-07-30 07:33:40 +0000

    Refactor and set up weaviate client for vector database

---
+++ src/db/weaviate.client.ts
@@ -0,0 +1,20 @@
+import weaviate, { WeaviateClient } from "weaviate-client";
+import { WeaviateStore } from "@langchain/weaviate";
+import { Embeddings } from "@services/embeddings.service";
+import '../../env.config'
+
+const weaviateURL = process.env.WEAVIATE_URL as string;
+const weaviateApiKey = process.env.WEAVIATE_API_KEY as string;
+
+
+export const client: WeaviateClient = await weaviate.connectToWeaviateCloud(weaviateURL, {
+    authCredentials: new weaviate.ApiKey(weaviateApiKey),
+  }
+)
+
+export const vectorStore = new WeaviateStore(Embeddings, {
+  client: client,
+  indexName: "ProjectData",
+});
+
+console.log("Weaviate client is ready?", await client.isReady())
--- vitest.config.mts
+++ vitest.config.mts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import './env.config.ts'
+
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
+    include: process.env.CI
+      ? ['__tests__/ci-tests/**/*.test.ts'] // Only include CI tests when CI is true
+      : ['__tests__/**/*.test.ts'], // Include all tests locally
     environment: 'jsdom',
   },
 })
