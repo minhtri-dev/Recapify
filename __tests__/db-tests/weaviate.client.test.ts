ac7d8d5ead6b0259be40f7388f4623b51127fd59
minhtri-dev <minhtritran.dev@gmail.com>
2025-07-30 07:33:40 +0000

    Refactor and set up weaviate client for vector database

---
+++ __tests__/db-tests/weaviate.client.test.ts
@@ -0,0 +1,42 @@
+
+import { vi, expect, test } from 'vitest'
+import { client, vectorStore } from '@db/weaviate.client'
+import type { Document } from "@langchain/core/documents";
+
+import '../../env.config'
+
+
+const originalImplementation = Array.isArray;
+
+// Patch Array.isArray to workaround issue with onnxruntime
+vi.spyOn(Array, "isArray").mockImplementation((value: unknown) => {
+  if (
+    value != null &&
+    value.constructor &&
+    (value.constructor.name === "Float32Array" ||
+      value.constructor.name === "BigInt64Array")
+  ) {
+    return true;
+  }
+  return originalImplementation(value);
+});
+
+test('Check Weaviate client connectivity', async () => {
+  const isReady = await client.isReady()
+  expect(isReady).toBe(true)
+})
+
+test('Add a document to the vectorStore', async () => {
+  const document: Document = {
+    pageContent: "The powerhouse of the cell is the mitochondria",
+    metadata: { source: "https://example.com" },
+  };
+
+  await vectorStore.addDocuments([document])
+
+  const results = await vectorStore.similaritySearch("powerhouse of the cell", 1);
+
+  expect(results).toHaveLength(1);
+  expect(results[0].pageContent).toBe(document.pageContent);
+  expect(results[0].metadata.source).toBe(document.metadata.source);
+})
--- src/services/embeddings.service.ts
+++ src/services/embeddings.service.ts
@@ -1,27 +1,4 @@
-import { pipeline } from '@xenova/transformers'
-import { type EmbeddingsInterface } from '@langchain/core/embeddings'
-
-async function getEmbedding(data: string[]): Promise<number[][]> {
-  try {
-    const extractor = await pipeline(
-      'feature-extraction',
-      'Xenova/all-MiniLM-L6-v2',
-    )
-    const response = await extractor(data, { pooling: 'mean', normalize: true })
-    return response.tolist()
-  } catch (error) {
-    console.error('Error fetching embeddings:', error)
-    throw new Error('Failed to get embeddings')
-  }
-}
-
-export class FacilityEmbeddings implements EmbeddingsInterface {
-  async embedDocuments(texts: string[]): Promise<number[][]> {
-    return getEmbedding(texts)
-  }
-
-  async embedQuery(text: string): Promise<number[]> {
-    const embeddings = await getEmbedding([text])
-    return embeddings[0]
-  }
-}
+import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/huggingface_transformers";
+export const Embeddings = new HuggingFaceTransformersEmbeddings({
+  model: "Xenova/all-mpnet-base-v2",
+});
