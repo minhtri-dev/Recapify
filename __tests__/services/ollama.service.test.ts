8c157948c8d4e8f4f4e6631e0a47fc01a48c0a54
minhtri-dev <minhtritran.dev@gmail.com>
2025-08-03 14:54:23 +0000

    Refactored code to use next auth instead of supabase auth

--- __tests__/services/ollama.service.test.ts
+++ __tests__/services/ollama.service.test.ts
@@ -6,5 +6,6 @@ test('Check Ollama service invocation', async () => {
   const res = await llm.invoke(
     'Tell me a fun fact about space. In less than 10 words.',
   )
+  console.log(res)
   expect(res).not.toBeNull()
 }, 30000)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Header, Footer } from '@components'
+import { Header, Footer, SignIn, SignOut } from '@components'
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Header />
       <p>Hello, world!</p>
+      <SignIn />
+      <SignOut />
       <Footer />
     </main>
   )
